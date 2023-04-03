28ac09e88f07d1d427416668dc14a3da16a931a2
StaffanHynge <staffanhynge@hotmail.com>
2023-04-03 10:59:22 +0000

    add a while loop to user function so it loops when you type the wrong name

--- run.py
+++ run.py
@@ -17,20 +17,23 @@ def get_user_name():
     """
     Get the user name.
     Check that the users name is one of the following in the list.
-    if not print an error.
+    if not print an error and the function loops.
     """
-
+    
     expected_names = ['Tommy', 'Jennie', 'Sara', 'Michael', 'Fred', 'Louise']
+    
     print("Please enter your name.")
     print(f"Your name should be one of the following: {', '.join(expected_names)}\n")
+    while True:
 
-    data_str = input("Enter your name here: ")
-    print(f"Your name is {data_str}")
+        data_str = input("Enter your name here: ")
+        print(f"Your name is {data_str}")
 
-    if data_str in expected_names:
-        print("Welcome!")
-    else:
-        print("Access Denied.")
+        if data_str in expected_names:
+            print("Welcome!")
+            return data_str
+        else:
+            print(f"Access Denied. Your name should be one of the following: {', '.join(expected_names)}\n")
 
 
 get_user_name()
